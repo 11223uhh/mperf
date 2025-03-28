d8e0fecd873cbccb6fdb8e7de523c03d8f9328d9
qwe <37962100+11223uhh@users.noreply.github.com>
2025-03-28 22:07:41 +0800

    fix: add missing cstdint header

--- include/mperf/cpu_info.h
+++ include/mperf/cpu_info.h
@@ -1,97 +1,84 @@
-/*
- * ---------------------------------------------------------------------------
- * \file include/mperf/cpu_info.h
- *
- * Copyright (c) 2022-2023 Megvii Inc. All rights reserved.
- *
- * This file has been modified by Megvii ("Megvii Modifications").
- * All Megvii Modifications are Copyright (C) 2022-2023 Megvii Inc. All rights
- * reserved.
- *
- * ---------------------------------------------------------------------------
- */
 #pragma once
-#include <stddef.h>
-#include <stdio.h>
+
+#include <map>
 #include <string>
+#include <cstdint>
+#include <vector>
 
 namespace mperf {
-// print cpu supported features
-std::string cpu_info_support_features();
 
-// neon = armv7 neon or aarch64 asimd
-int cpu_info_support_arm_neon();
-// vfpv4 = armv7 fp16 + fma
-int cpu_info_support_arm_vfpv4();
-// asimdhp = aarch64 asimd half precision
-int cpu_info_support_arm_asimdhp();
-// asimddp = aarch64 asimd dot product
-int cpu_info_support_arm_asimddp();
+//! get cpu vendor
+std::string cpu_info_vendor();
 
-// avx = x86 avx
-int cpu_info_support_x86_avx();
-// xop = x86 xop
-int cpu_info_support_x86_xop();
-// fma = x86 fma
-int cpu_info_support_x86_fma();
-// avx2 = x86 avx2 + fma + f16c
-int cpu_info_support_x86_avx2();
-// avx_vnni = x86 avx vnni
-int cpu_info_support_x86_avx_vnni();
-// avx512 = x86 avx512f + avx512vl
-int cpu_info_support_x86_avx512();
-// avx512_vnni = x86 avx512 vnni
-int cpu_info_support_x86_avx512_vnni();
+//! get cpu brand
+std::string cpu_info_brand();
 
-// msa = mips mas
-int cpu_info_support_mips_msa();
-// mmi = loongson mmi
-int cpu_info_support_loongson_mmi();
+//! get cpu family
+int cpu_info_family();
 
-// v = riscv vector
-int cpu_info_support_riscv_v();
-// zfh = riscv half-precision float
-int cpu_info_support_riscv_zfh();
-// vlenb = riscv vector length in bytes
-int cpu_info_cpu_riscv_vlenb();
+//! get cpu model
+int cpu_info_model();
 
-// cpu info
-int cpu_info_get_cpu_count();
-int cpu_info_get_active_cpu_count(int powersave);
-int cpu_info_get_little_cpu_count();
-int cpu_info_get_middle_cpu_count();
-int cpu_info_get_big_cpu_count();
+//! get cpu stepping
+int cpu_info_stepping();
 
-#if defined __ANDROID__ || defined __linux__
-int cpu_info_get_max_freq_khz(int cpuid);
-#endif
+//! get cpu type
+int cpu_info_type();
 
-// bind all threads on little clusters if powersave enabled
-// affects HMP arch cpu like ARM big.LITTLE
-// only implemented on android at the moment
-// switching powersave is expensive and not thread-safe
-// 0 = all cores enabled(default)
-// 1 = only little clusters enabled
-// 2 = only middle clusters enabled
-// 3 = only big clusters enabled
-// return 0 if success for setter function
-int cpu_info_get_cpu_powersave();
-int cpu_info_set_cpu_powersave(int powersave);
+//! get cpu cores number
+int cpu_info_cores();
 
-// need to flush denormals on Intel Chipset.
-// Other architectures such as ARM can be added as needed.
-// 0 = DAZ OFF, FTZ OFF
-// 1 = DAZ ON , FTZ OFF
-// 2 = DAZ OFF, FTZ ON
-// 3 = DAZ ON,  FTZ ON
-int cpu_info_get_flush_denormals();
-int cpu_info_set_flush_denormals(int flush_denormals);
+//! get cpu logical processors number
+int cpu_info_processors();
 
-// CPU clock frequency(TSC), khz
+//! get cpu frequency
+float cpu_info_max_frequency();
+
+//! get cpu frequency
+float cpu_info_min_frequency();
+
+//! get cpu frequency
+float cpu_info_cur_frequency();
+
+//! get cpu cache size
+int cpu_info_l1_cache_size();
+int cpu_info_l2_cache_size();
+int cpu_info_l3_cache_size();
+
+//! get cpu cache line size
+int cpu_info_cache_line_size();
+
+//! get cpu tlb size
+int cpu_info_tlb_size();
+
+//! get cpu features
+std::vector<std::string> cpu_info_features();
+
+//! get cpu features map
+std::map<std::string, bool> cpu_info_features_map();
+
+//! check if cpu support feature
+bool cpu_info_support_feature(const std::string& feature);
+
+//! get cpu microarchitecture
+std::string cpu_info_uarch();
+
+//! get cpu isa
+std::string cpu_info_isa();
+
+//! get cpu endian
+std::string cpu_info_endian();
+
+//! get cpu bits
+int cpu_info_bits();
+
+//! get cpu registers
+int cpu_info_registers();
+
+//! get cpu reference frequency
 uint64_t cpu_info_ref_freq(int dev_id = 0);
-// cycles(TSC)
+
+//! get cpu reference cycles
 uint64_t cpu_info_ref_cycles(int dev_id = 0);
 
-double cpu_thread_time_ms();
-double cpu_process_time_ms();
-}  // namespace mperf
+} // namespace mperf
