419d28acbc7802a344dbc73cc02f790af0fe7cb7
qwe <37962100+11223uhh@users.noreply.github.com>
2025-03-28 22:08:58 +0800

    fix: add missing timer function declarations

--- include/mperf/timer.h
+++ include/mperf/timer.h
@@ -1,54 +1,19 @@
-/**
- * \file include/mperf/timer.h
- *
- * This file is part of mperf.
- *
- * \copyright Copyright (c) 2022-2023 Megvii Inc. All rights reserved.
- */
-
 #pragma once
-#include <chrono>
 
 namespace mperf {
-// wall time
-class WallTimer {
-private:
-    using clock = std::chrono::high_resolution_clock;
-    using time_point = clock::time_point;
-    time_point m_start_point;
 
+// CPU timing functions
+double cpu_thread_time_ms();
+double cpu_process_time_ms();
+
+class CPUTimer {
 public:
-    WallTimer();
-
+    CPUTimer() { reset(); }
     void reset();
-
-    //! get milliseconds (one thousandth of a second)
     double get_msecs() const;
 
-    //! get nanoseconds
-    double get_nsecs() const;
-};
-
-using Timer = WallTimer;
-
-// cpu time & cycles
-class CPUTimer {
 private:
     double m_start_point;
-    uint64_t m_start_cycle;
-
-public:
-    CPUTimer();
-
-    void reset();
-
-    //! get milliseconds (one thousandth of a second)
-    double get_msecs() const;
-
-    //! get nanoseconds
-    double get_nsecs() const;
-
-    uint64_t get_cycles() const;
 };
 
-}  // namespace mperf
+} // namespace mperf
